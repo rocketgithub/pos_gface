363ef5b0110dc0213d2e27b3329c110aac73b7a1
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2019-12-13 15:02:44 +0000

    Validación al hacer cierre

---
+++ models/pos_session.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+
+    def action_pos_session_close(self):
+        for session in self:
+            if session.config_id.iface_invoicing:
+                orders = session.order_ids.filtered(lambda order: order.state != 'invoiced' and order.amount_total > 0)
+                if len(orders) > 0:
+                    raise ValidationError('Tiene pedidos sin factura, no puede cerrar sesión mientras no haya facturado todos los pedidos.')
+                for order in session.order_ids.filtered(lambda order: order.state == 'invoiced'):
+                    if order.invoice_id.state != 'open' and not order.invoice_id.firma_fel:
+                        raise ValidationError('La factura del pedido {} no está firmada, por ingrese a la factura valídela para poder cerrar sesión.'.format(order.name))
+
+        res = super(PosSession, self).action_pos_session_close()
