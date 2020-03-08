eea126d56c3d958a14687f177ad92b7d538638cb
Jefferson Silva <jeffer.leo93@gmail.com>
2020-03-08 14:23:26 -0600

    Agregar series en lineas de factura

--- models/pos_order.py
+++ models/pos_order.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from odoo import models, fields, api, _
+import logging
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
@@ -8,3 +9,13 @@ class PosOrder(models.Model):
     firma_fel = fields.Char('Firma FEL', related='invoice_id.firma_fel')
     serie_fel = fields.Char('Serie FEL', related='invoice_id.serie_fel')
     numero_fel = fields.Char('Numero FEL', related='invoice_id.numero_fel')
+
+    @api.model
+    def _action_create_invoice_line(self, line=False, invoice_id=False):
+        res = super(PosOrder, self)._action_create_invoice_line(line,invoice_id)
+        if line.pack_lot_ids:
+            lotes = [linea.lot_name for linea in line.pack_lot_ids]
+            separador = ' '
+            conjunto_lotes = separador.join(lotes)
+            res.write({'name':  res.name +' '+str(conjunto_lotes)})
+        return res
